1053e4cefec9f7a0a68cae2079b4ca1c2999096e
Omar7001-B <Omar.20375710@compit.aun.edu.eg>
2024-03-19 05:11:39 +0200

    Greedy Search

---
+++ Informed Search Algorithms/A Star/Greedy Search.py
@@ -0,0 +1,43 @@
+graph = {
+    'S': [('A', 1), ('B', 4)],
+    'A': [('B', 2), ('C', 5), ('G', 12)],
+    'B': [('C', 2)],
+    'C': [('G', 3)]
+}
+
+H_table = {
+    'S': 7,
+    'A': 6,
+    'B': 4,
+    'C': 2,
+    'G': 0
+}
+def herustic_cost(path):
+    last_node = path[-1][0]
+    # g_cost = 0
+    # for node, cost in path:
+    #     g_cost += cost
+    #h_cost = H_table[last_node]
+    # f_cost = g_cost + h_cost
+    return H_table[last_node]
+
+
+def greedy(start, target):
+    visited = []
+    queue = [[(start, 0)]]
+    while queue:
+        queue.sort(key=herustic_cost)
+        path = queue.pop(0)
+        node = path[-1][0]
+        if node not in visited:
+            visited.append(node)
+            if node == target:
+                return path
+            else:
+                children = graph.get(node, [])
+                for child in children:
+                    queue.append(path + [child])
+    return None
+
+
+print("Solution is ", greedy('S', 'G'))
